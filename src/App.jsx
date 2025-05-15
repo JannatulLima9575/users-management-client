ba06c3325573a32489942d6c981448299432bff1
Jannatul <limajannat1675@gmail.com>
2025-05-15 21:35:04 +0600

    added a form strucure

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import './App.css'
+import User from './Components/User';
+
+const usersPromise = fetch('http://localhost:3000/users').then(res => res.json());
 
 function App() {
 
   return (
     <>
       <h1>Users Management Client</h1>
+      <User usersPromise={usersPromise}></User>
     </>
   )
 }
